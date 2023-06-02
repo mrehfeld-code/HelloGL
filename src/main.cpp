8f42d418e741aaed44e408d0b3bb44a50dfb3c15
M. Rehfeld <m.rehfeld.web@gmx.net>
2023-06-02 13:46:12 +0200

    first setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/* include basic io functions*/
+#include <iostream>
+
+/* include SDL3 */
+#include "SDL3/SDL.h"
+/* include GLEW */
+#include "GL/glew.h"
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
